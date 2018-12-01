b269134e8ce2c5e60ff75a198c031e1fa26e447b
Erik Hoffman <erik@MacBook-Pro.local>
2018-12-01 09:51:55 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/webhook', (req, res) => res.send('Hello Webhook World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
