9f101a32ff9e4fc2b42331e6410adf90264c3367
Erik Hoffman <erik@MacBook-Pro.local>
2018-12-02 10:46:02 -0500

    working endpoint

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const request = require('request')
+const async = require('async')
 const bodyParser = require('body-parser')
 const app = express()
 
+const headerMiddleware = require('./middleware/header.middleware');
+
 const primaryKey = '7e5c0bb85aff4345847c352b80654e9e';
 const secondaryKey = 'd19538df049d46e7b2b3c6811e745cdd';
+const port = process.env.PORT || 1337;
+
+app.use(headerMiddleware);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -82,6 +88,8 @@ app.get('/accounts/:account_id/transactions', function (req, res) {
         url: 'https://api.azureminilab.com/accounts/' + req.params.account_id + '/transactions'
     }, function(err, response, body) {
         let json = JSON.parse(body);
+        var txnsArray = json[50];
+        console.log(txnsArray);
         res.send(json);
     })
 });
@@ -100,37 +108,101 @@ app.get('/transactions/:transactions_id', function (req, res) {
 });
 
 app.get('/shortfall/:id', function (req, res) {
-    var debitsWeekOne = 0;
-    var debitsWeekTwo = 0;
-    var debitsWeekThree = 0;
-    var debitsWeekFour = 0;
-    var creditsWeekOne = 0;
-    var creditsWeekTwo = 0;
-    var creditsWeekThree = 0;
-    var creditsWeekFour = 0;
-    request.get({
-        headers: {
-            'content-type': 'application/json',
-            'Ocp-Apim-Subscription-Key': primaryKey
-        },
-        url: 'https://api.azureminilab.com/customers/' + req.params.id + '/accounts'
-    }).then(function(body) {
-        /* Grab all of the accounts*/
-        let accounts = JSON.parse(body);
+    var debits = [0, 0, 0, 0];
+    var credits = [0, 0, 0, 0];
+
+    /* Hard coded to skip some implementation details which are complex
+       Grabs 52 weeks back from this date, and it is considered a week 1 start of month 14 essentially
+       So Saturday December 1st, is the 7th day of the 4th week of the 13th month */
+    var mostRecentSunday = new Date("12/2/2018");
+
+    getAccounts(req, res)
+    .then(accounts => {
         /* For each account add up all of the transactions to debit or credit var
          based on what week it falls into for 13 month calendar
         */
-        async.map(accounts , function(account, callback) {
-            request.get("https://api.azureminilab.com/accounts/"+account.id+'/transactions', function(body) {
-                res.send(JSON.parse(body));
+        var accountsJSON = JSON.parse(accounts);
+
+        async.map(accountsJSON , (account, callback) => {
+            return request.get({
+                headers: {
+                    'content-type': 'application/json',
+                    'Ocp-Apim-Subscription-Key': primaryKey
+                },
+                url: `https://api.azureminilab.com/accounts/${account.id}/transactions`
+            }, (err, response) => {
+                try { 
+                    getTransactions(req, res, account)
+                } catch(e) {
+                    console.log('Error caught');
+                }
+                callback(false, JSON.parse(response.body));
+            });
+        }, (err, response) => {
+            if (err) {
+                throw new Error('oops');
+            }
+
+            for (var j = 0; j < response.length; j++) {
+                var transactionsJSON = response[j];
+                for (var i = 0; i < transactionsJSON.length; i++) {
+                    var txnDate = new Date(transactionsJSON[i].posted_date);
+                    var timeDiff = Math.abs(mostRecentSunday.getTime() - txnDate.getTime());
+                    var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+                    var week = Math.floor((daysDiff / 7 * -1) % 4 * -1);
+                    if (daysDiff < 1000) {
+                        if (transactionsJSON[i].type.indexOf("DEBIT") !== -1) {
+                            debits[week] += parseInt(transactionsJSON[i].transaction_value);
+                        } else if (transactionsJSON[i].type.indexOf("CREDIT") !== -1) {
+                            credits[week] += parseInt(transactionsJSON[i].transaction_value);  
+                        }
+                    }
+                }
+            }
+
+            console.log('Total debits for week 1: ' + debits[0]);
+            console.log('Total credits for week 1: ' + credits[0]);
+            var averageShortfall = [0, 0, 0, 0];
+            for (var i = 0; i < 4; i++) {
+                averageShortfall[i] = (credits[i] - debits[i])/13;
+            }
+
+            var shortFallWeekOne = parseInt(JSON.stringify(averageShortfall[0]));
+            res.send({
+                shortFall: shortFallWeekOne
             });
         });
-
-        /* Master calculation to find out which week 1-4 it is:
-            floor( (364 - daysDiff(mostRecentSunday, dayOfTransaction)) / 7) % 4 = which week to average*/
-    })
+    });
 })
 
+function getAccounts(req, res) {
+    return new Promise(resolve => {
+        request.get({
+            headers: {
+                'content-type': 'application/json',
+                'Ocp-Apim-Subscription-Key': primaryKey
+            },
+            url: 'https://api.azureminilab.com/customers/' + req.params.id + '/accounts'
+        }, function (error, response, body) {
+            if(!error)
+                resolve(body);
+        })
+    })
+}
 
-var port = process.env.PORT || 1337;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+function getTransactions(req, res, account) { 
+    return new Promise(resolve => {
+        request.get({
+            headers: {
+                'content-type': 'application/json',
+                'Ocp-Apim-Subscription-Key': primaryKey
+            },
+            url: "https://api.azureminilab.com/accounts/"+account.id+'/transactions'
+        }, function (error, response, body) {
+            if(!error)
+                resolve(body);
+        })
+    })
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
